609f3b60daa3c9150e799d2d081139e60b27ab99
Salsa Nabila <salsanabila2018@gmail.com>
2026-07-15 19:26:37 +0700

    Revert "feat(backend): initialize Express TypeScript server"

--- backend/src/routes/health.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getHealthStatus } from '../controllers/health.controller';
-
-const healthRouter = Router();
-
-healthRouter.get('/', getHealthStatus);
-
-export default healthRouter;
--- backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import healthRouter from "./health.route";
-
-const apiRouter = Router();
-
-apiRouter.use("/health", healthRouter);
-
-export default apiRouter;
